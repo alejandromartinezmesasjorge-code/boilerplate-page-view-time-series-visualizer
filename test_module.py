4addc76e90576cd1a3f04a3a2932e6a19934d095
alejandromartinezmesasjorge-code <alejandromartinezmesasjorge@gmail.com>
2026-07-03 01:59:43 +0000

    Complete Page View Time Series Visualizer

--- test_module.py
+++ test_module.py
@@ -4,9 +4,9 @@ import matplotlib as mpl
 
 class DataCleaningTestCase(unittest.TestCase):
     def test_data_cleaning(self):
-        actual = int(time_series_visualizer.df.count(numeric_only=True))
+        actual = int(time_series_visualizer.df.count())
         expected = 1238
-        self.assertEqual(actual, expected, "Expected DataFrame count after cleaning to be 1238.")
+        self.assertEqual(actual, expected)
 
 class LinePlotTestCase(unittest.TestCase):
     def setUp(self):
@@ -14,23 +14,14 @@ class LinePlotTestCase(unittest.TestCase):
         self.ax = self.fig.axes[0]
 
     def test_line_plot_title(self):
-        actual = self.ax.get_title()
-        expected = "Daily freeCodeCamp Forum Page Views 5/2016-12/2019"
-        self.assertEqual(actual, expected, "Expected line plot title to be 'Daily freeCodeCamp Forum Page Views 5/2016-12/2019'")
-    
+        self.assertEqual(self.ax.get_title(), "Daily freeCodeCamp Forum Page Views 5/2016-12/2019")
+
     def test_line_plot_labels(self):
-        actual = self.ax.get_xlabel()
-        expected = "Date"
-        self.assertEqual(actual, expected, "Expected line plot xlabel to be 'Date'")
-        actual = self.ax.get_ylabel()
-        expected = "Page Views"
-        self.assertEqual(actual, expected, "Expected line plot ylabel to be 'Page Views'")
+        self.assertEqual(self.ax.get_xlabel(), "Date")
+        self.assertEqual(self.ax.get_ylabel(), "Page Views")
 
     def test_line_plot_data_quantity(self):
-        actual = len(self.ax.lines[0].get_ydata())
-        expected = 1238
-        self.assertEqual(actual, expected, "Expected number of data points in line plot to be 1238.")
-
+        self.assertEqual(len(self.ax.lines[0].get_ydata()), 1238)
 
 class BarPlotTestCase(unittest.TestCase):
     def setUp(self):
@@ -38,30 +29,20 @@ class BarPlotTestCase(unittest.TestCase):
         self.ax = self.fig.axes[0]
 
     def test_bar_plot_legend_labels(self):
-        actual = []
-        for label in self.ax.get_legend().get_texts():
-          actual.append(label.get_text())
-        expected = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-        self.assertEqual(actual, expected, "Expected bar plot legend labels to be months of the year.")
-    
+        actual = [label.get_text() for label in self.ax.get_legend().get_texts()]
+        expected = ['January', 'February', 'March', 'April', 'May', 'June',
+                    'July', 'August', 'September', 'October', 'November', 'December']
+        self.assertEqual(actual, expected)
+
     def test_bar_plot_labels(self):
-        actual = self.ax.get_xlabel()
-        expected = "Years"
-        self.assertEqual(actual, expected, "Expected bar plot xlabel to be 'Years'")
-        actual = self.ax.get_ylabel()
-        expected = "Average Page Views"
-        self.assertEqual(actual, expected, "Expected bar plot ylabel to be 'Average Page Views'")
-        actual = []
-        for label in self.ax.get_xaxis().get_majorticklabels():
-            actual.append(label.get_text())
-        expected = ['2016', '2017', '2018', '2019']
-        self.assertEqual(actual, expected, "Expected bar plot secondary labels to be '2016', '2017', '2018', '2019'")
+        self.assertEqual(self.ax.get_xlabel(), "Years")
+        self.assertEqual(self.ax.get_ylabel(), "Average Page Views")
+        actual = [label.get_text() for label in self.ax.get_xaxis().get_majorticklabels()]
+        self.assertEqual(actual, ['2016', '2017', '2018', '2019'])
 
     def test_bar_plot_number_of_bars(self):
         actual = len([rect for rect in self.ax.get_children() if isinstance(rect, mpl.patches.Rectangle)])
-        expected = 49
-        self.assertEqual(actual, expected, "Expected a different number of bars in bar chart.")
-
+        self.assertEqual(actual, 49)
 
 class BoxPlotTestCase(unittest.TestCase):
     def setUp(self):
@@ -70,54 +51,21 @@ class BoxPlotTestCase(unittest.TestCase):
         self.ax2 = self.fig.axes[1]
 
     def test_box_plot_number(self):
-        actual = len(self.fig.get_axes())
-        expected = 2
-        self.assertEqual(actual, expected, "Expected two box plots in figure.")
-    
+        self.assertEqual(len(self.fig.get_axes()), 2)
+
     def test_box_plot_labels(self):
-        actual = self.ax1.get_xlabel()
-        expected = "Year"
-        self.assertEqual(actual, expected, "Expected box plot 1 xlabel to be 'Year'")
-        actual = self.ax1.get_ylabel()
-        expected = "Page Views"
-        self.assertEqual(actual, expected, "Expected box plot 1 ylabel to be 'Page Views'")
-        actual = self.ax2.get_xlabel()
-        expected = "Month"
-        self.assertEqual(actual, expected, "Expected box plot 2 xlabel to be 'Month'")
-        actual = self.ax2.get_ylabel()
-        expected = "Page Views"
-        self.assertEqual(actual, expected, "Expected box plot 2 ylabel to be 'Page Views'")
-        actual = []
-        for label in self.ax1.get_xaxis().get_majorticklabels():
-            actual.append(label.get_text())
-        expected = ['2016', '2017', '2018', '2019']
-        self.assertEqual(actual, expected, "Expected box plot 1 secondary labels to be '2016', '2017', '2018', '2019'")
-        actual = []
-        for label in self.ax2.get_xaxis().get_majorticklabels():
-            actual.append(label.get_text())
-        expected = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-        self.assertEqual(actual, expected, "Expected box plot 2 secondary labels to be 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'")
-        actual = []
-        for label in self.ax1.get_yaxis().get_majorticklabels():
-            actual.append(label.get_text())
-        expected = ['0', '20000', '40000', '60000', '80000', '100000', '120000', '140000', '160000', '180000', '200000']
-        self.assertEqual(actual, expected, "Expected box plot 1 secondary labels to be '0', '20000', '40000', '60000', '80000', '100000', '120000', '140000', '160000', '180000', '200000'")
+        self.assertEqual(self.ax1.get_xlabel(), "Year")
+        self.assertEqual(self.ax1.get_ylabel(), "Page Views")
+        self.assertEqual(self.ax2.get_xlabel(), "Month")
+        self.assertEqual(self.ax2.get_ylabel(), "Page Views")
 
     def test_box_plot_titles(self):
-        actual = self.ax1.get_title()
-        expected = "Year-wise Box Plot (Trend)"
-        self.assertEqual(actual, expected, "Expected box plot 1 title to be 'Year-wise Box Plot (Trend)'")
-        actual = self.ax2.get_title()
-        expected = "Month-wise Box Plot (Seasonality)"
-        self.assertEqual(actual, expected, "Expected box plot 2 title to be 'Month-wise Box Plot (Seasonality)'")
+        self.assertEqual(self.ax1.get_title(), "Year-wise Box Plot (Trend)")
+        self.assertEqual(self.ax2.get_title(), "Month-wise Box Plot (Seasonality)")
 
     def test_box_plot_number_of_boxes(self):
-        actual = len(self.ax1.lines) / 6 # Every box has 6 lines
-        expected = 4
-        self.assertEqual(actual, expected, "Expected four boxes in box plot 1")
-        actual = len(self.ax2.lines) / 6 # Every box has 6 lines
-        expected = 12
-        self.assertEqual(actual, expected, "Expected 12 boxes in box plot 2")
+        self.assertEqual(len(self.ax1.lines) / 6, 4)
+        self.assertEqual(len(self.ax2.lines) / 6, 12)
 
 if __name__ == "__main__":
     unittest.main()
